fccf8c0627c308a4eccec775ef4907a52151d28f
alijalal-sys <ajmabilal@gmail.com>
2020-01-23 14:43:59 +0100

    User schema & model & functions done!

--- Models/userModel.js
+++ Models/userModel.js
@@ -27,15 +27,46 @@ var userSchema = mongoose.Schema({
   }
 })
 
-var User = mongoose.model('User', userSchema);
+var User = new mongoose.model('User', userSchema);
 
-var insertUser = (userData) => {
-  return User.findOne(userData)
-    .then(_ => {
-      return User.create(userData)
-    })
-    .catch(err => {
-      return err;
-    })
+
+/*******************************
+ *******************************
+ ********** HELPERS ************
+ *******************************
+ *******************************/
+
+/**
+ * @function Helpers ...
+ * @param {*} All 
+ * @returns all of them return a promise to be handled later on
+ */
+
+
+// This function works now and ready to export
+// but we might add the biometrically so it needs more work!
+module.exports.insertUser = ( userData ) => {
+  return User.create(userData);
 }
 
+// Delete user ready to export it recieve user id and delete it from db ..
+module.exports.deleteUser = ( userId ) => {
+  return User.findOneAndDelete({ _id: userId })
+}
+
+// This function accept userId, as target and object criteria
+// ex { name: 'Ali jalal' } this will target the user by id
+// and set his name to the object criteria ...
+module.exports.updateUser = (userId, objectCriteria) => {
+  return User.findByIdAndUpdate(userId, objectCriteria)
+}
+
+// Retreve user by Id
+module.exports.getUser = ( userId ) => {
+  return User.findOne({ _id: userId })
+}
+
+// Retrieve all users
+module.exports.getUsers = () => {
+  return User.find()
+}
--- server/server.js
+++ server/server.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const {User} = require('../Models/userModel') 
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/the-fifth', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+}, (err) => {
+    if (err) {
+        console.log('Error while connecting ..' + err)
+    } else {
+        console.log('Connected to Database')
+    }
+})
+
+app.listen(8080, _ => {
+    console.log('Listening to 8080 ...');
+})
