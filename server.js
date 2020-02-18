01fa9aa06bb7e8d7faa2f2bca650c797670088de
Abobker Elaghel <57757187+AbobkerElaghel@users.noreply.github.com>
2020-02-18 13:05:58 +0100

    done with the word file

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const typeDefs = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers');
 // const PORT = process.env.PORT || 4000;
 const app = express();
-
+const cors = require('cors')
 
 app.use(express.json());
-
+app.use(cors());
 
 const {
   createServer
